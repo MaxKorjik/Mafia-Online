7e5be0636b3ea738ba1cddc46d94c62779aa9146
yyaaaarriikk <lavrenjuk0379@gmail.com>
2025-04-26 20:58:31 +0300

    guest in main

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
+    guestname = Column(String, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
     email = Column(String, unique=True, index=True)
     friends = Column(JSON, default = list)
