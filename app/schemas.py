bcc2a7a750ec7d1a35cfca69c9a8536c03783773
MaxKorjik <koshchinmaxim06@gmail.com>
2025-04-28 18:17:03 +0300

    Rebase of code

--- app/schemas.py
+++ app/schemas.py
@@ -1,23 +1,31 @@
 from pydantic import BaseModel
-from typing import List 
+from typing import List, Optional, Union
 
 class UserBase(BaseModel):
-    id: int 
-    username: str 
+    id: int
+    username: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class UserCreate(BaseModel):
-    username: str 
-    password: str 
-    email: str 
+    username: str
+    password: str
+    email: str
 
 class UserResponse(BaseModel):
-    id: int 
-    username: str 
-    email: str 
+    id: int
+    username: str
+    email: str
     friends: List[int] = []
+    matches: int = 0
+    survivor_matches: int = 0
+    mafia_matches: int = 0
+    is_host: bool = False
+    is_admin: bool = False
+
+    class Config:
+        from_attributes = True
 
 class UserLogin(UserCreate):
     pass
@@ -27,7 +35,26 @@ class Token(BaseModel):
     token_type: str
 
 class ChatMessage(BaseModel):
-    message: str 
+    message: str
 
 class AddFriend(BaseModel):
     friend_id: int
+
+class RoomBase(BaseModel):
+    name: str
+    is_private: bool = False
+    min_players_number : int = 6
+    max_players_number: int = 6
+    password: Optional[str] = None
+
+class RoomCreate(RoomBase):
+    pass
+
+class RoomResponse(RoomBase):
+    id: int
+    owner: int
+    players_number: int = 0
+    is_active: bool = True
+
+    class Config:
+        from_attributes = True
